4b3e67cf805f29975f337b62292d2fd38117dba4
fraolb <Fraolbereket@gmail.com>
2025-02-16 16:18:25 +0300

    update login page

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ import {
 } from "@getpara/evm-wallet-connectors";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { sepolia, celo, mainnet, polygon } from "wagmi/chains";
+import { WalletType } from "@getpara/react-sdk";
 
 const queryClient = new QueryClient();
 
@@ -26,9 +27,15 @@ export default function Home() {
   // Handle login event
   const handleLogin = async () => {
     try {
-      const user = await para.getUserId(); // Fetch user data
+      const user = await para.getUserId();
       if (user) {
-        router.push("/dashboard"); // Redirect user after login
+        console.log("User logged in:", user);
+
+        // Fetch and log connected wallets
+        const evmWallets = await para.getWalletsByType(WalletType.EVM);
+        console.log("EVM Wallets after login:", evmWallets);
+
+        router.push("/dashboard");
       }
     } catch (error) {
       console.error("Login error:", error);
@@ -36,7 +43,11 @@ export default function Home() {
   };
 
   useEffect(() => {
-    handleLogin();
+    const interval = setInterval(() => {
+      handleLogin();
+    }, 15000);
+
+    return () => clearInterval(interval);
   }, []);
 
   return (
@@ -57,8 +68,20 @@ export default function Home() {
       >
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
           <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-            <div>
-              <button onClick={() => setIsOpen(true)}>Sign in with Para</button>
+            <div className="flex justify-center">
+              <img
+                src="https://mintlify.s3.us-west-1.amazonaws.com/getpara/logo/light.svg"
+                width={300}
+                height={300}
+              />
+            </div>
+            <div className="flex justify-center w-full ">
+              <button
+                onClick={() => setIsOpen(true)}
+                className="border border-black-500 p-2 px-4 rounded-md bg-orange-600 text-white"
+              >
+                Sign in with Para
+              </button>
 
               <ParaModal
                 para={para}
@@ -82,14 +105,6 @@ export default function Home() {
                 onRampTestMode={true}
               />
             </div>
-            <button
-              onClick={() => {
-                router.push("/dashboard");
-              }}
-            >
-              {" "}
-              route
-            </button>
           </main>
         </div>
       </ParaEvmProvider>
