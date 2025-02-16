edfbc23fcffde1987cd2542c3e28bbba2e51bcf7
fraolb <Fraolbereket@gmail.com>
2025-02-16 23:43:05 +0300

    add a function to add smart account from biconomy

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -9,7 +9,6 @@ import {
   createBicoPaymasterClient,
   toNexusAccount,
 } from "@biconomy/abstractjs";
-import { baseSepolia } from "viem/chains";
 import { http, parseEther } from "viem";
 import para from "../../para";
 import { privateKeyToAccount, generatePrivateKey } from "viem/accounts";
@@ -17,7 +16,7 @@ import { privateKeyToAccount, generatePrivateKey } from "viem/accounts";
 import { ParaEthersV5Signer } from "@getpara/ethers-v5-integration";
 import { ethers } from "ethers";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { sepolia, celo, mainnet, polygon } from "wagmi/chains";
+import { sepolia, celo, mainnet, polygon, baseSepolia } from "wagmi/chains";
 import {
   ParaEvmProvider,
   coinbaseWallet,
@@ -26,6 +25,11 @@ import {
   rainbowWallet,
   zerionWallet,
 } from "@getpara/evm-wallet-connectors";
+import { ParaEthersSigner } from "@getpara/ethers-v6-integration";
+import {
+  BiconomySmartAccountV2,
+  DEFAULT_ENTRYPOINT_ADDRESS,
+} from "@biconomy/account";
 
 const queryClient = new QueryClient();
 
@@ -40,6 +44,51 @@ const SignWithBiconomy: React.FC = () => {
   const [amount, setAmount] = useState<string>("0");
   const [recipient, setRecipient] = useState("");
   const [loading, setLoading] = useState(false);
+  const [smartAccount, setSmartAccount] = useState<
+    BiconomySmartAccountV2 | undefined
+  >();
+  const [smartAddress, setSmartAddress] = useState<`0x${string}` | null>();
+
+  const provider = new ethers.JsonRpcProvider(
+    "https://base-sepolia.infura.io/v3/1936342871af407688f7f4381bd50ceb"
+  );
+
+  const bundlerUrl =
+    "https://bundler.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44";
+  const paymasterUrl =
+    "https://paymaster.biconomy.io/api/v2/84532/F7wyL1clz.75a64804-3e97-41fa-ba1e-33e98c2cc703";
+
+  const createSmartAccount = async () => {
+    if (!para.getUserId()) {
+      alert("Please connect your Para account.");
+      return;
+    }
+
+    setLoading(true);
+    try {
+      const ethersSigner = new ParaEthersSigner(para, provider);
+
+      if (!ethersSigner) {
+        throw new Error("Failed to retrieve signer for wallet.");
+      }
+
+      let biconomySmartAccount = await BiconomySmartAccountV2.create({
+        signer: ethersSigner,
+        chainId: baseSepolia.id,
+        bundlerUrl: bundlerUrl,
+        biconomyPaymasterApiKey: paymasterUrl,
+        entryPointAddress: DEFAULT_ENTRYPOINT_ADDRESS,
+      });
+
+      setSmartAccount(biconomySmartAccount);
+      const address = await biconomySmartAccount.getAddress();
+      setSmartAddress(address);
+    } catch (error) {
+      console.error("Error during transaction: ", error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   const handleTransaction = async () => {
     if (!recipient) {
@@ -90,24 +139,27 @@ const SignWithBiconomy: React.FC = () => {
       console.log("Using Wallet ID:", walletId);
 
       // Get the signer for this wallet
-      const provider = new ethers.providers.JsonRpcProvider(
-        "https://base-sepolia.infura.io/v3/1936342871af407688f7f4381bd50ceb"
-      );
 
-      const ethersSigner = new ParaEthersV5Signer(para, provider, walletId);
+      const ethersSigner = new ParaEthersSigner(para, provider);
       const address = await ethersSigner.getAddress();
       console.log(
         "Signer Address:",
         ethersSigner,
         ethersSigner.getAddress(),
+        ethersSigner.getNonce(),
+        "wallet ",
+        para.getWallets()[0],
+
         address
       );
+      const wallets = para.getWallets();
+      const firstWallet = Object.values(wallets)[0];
 
       const signer = {
         ...ethersSigner,
         address: (await ethersSigner.getAddress()) as `0x${string}`, // ✅ Add explicit address property
         getAddress: async () => ethersSigner.getAddress(),
-        signTransaction: async (tx: ethers.providers.TransactionRequest) =>
+        signTransaction: async (tx: ethers.TransactionRequest) =>
           ethersSigner.signTransaction(tx),
         signMessage: async (message: string | Uint8Array) =>
           ethersSigner.signMessage(message),
@@ -118,20 +170,18 @@ const SignWithBiconomy: React.FC = () => {
         ) => ethersSigner.signTypedData(domain, types, value),
       };
 
+      const privateKey = generatePrivateKey();
+      const account = privateKeyToAccount(`${privateKey}`);
+
       // Ensure the signer is valid
       if (!ethersSigner) {
         throw new Error("Failed to retrieve signer for wallet.");
       }
 
-      const bundlerUrl =
-        "https://bundler.biconomy.io/api/v3/84532/nJPK7B3ru.dd7f7861-190d-41bd-af80-6877f74b8f44";
-      const paymasterUrl =
-        "https://paymaster.biconomy.io/api/v2/84532/F7wyL1clz.75a64804-3e97-41fa-ba1e-33e98c2cc703";
-
       // Use the recovered signer from Para, instead of generating a new one
       const nexusClient = createSmartAccountClient({
         account: await toNexusAccount({
-          signer: signer, // Use signer retrieved from Para
+          signer: account,
           chain: baseSepolia,
           transport: http(),
         }),
@@ -154,6 +204,7 @@ const SignWithBiconomy: React.FC = () => {
       const receipt = await nexusClient.waitForTransactionReceipt({ hash });
       console.log("Transaction receipt: ", receipt);
       setTransactionReceipt(receipt);
+      setLoading(false);
     } catch (error) {
       console.error("Error during transaction: ", error);
     } finally {
@@ -175,7 +226,7 @@ const SignWithBiconomy: React.FC = () => {
         }
       };
       checkLogin();
-    }, 15000);
+    }, 55000);
 
     return () => clearInterval(interval);
   }, []);
@@ -226,54 +277,82 @@ const SignWithBiconomy: React.FC = () => {
             />
           </div>
           <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-
-          <h2 className="text-xl font-semibold mb-4 text-center">
-            Send Gasless Transaction
-          </h2>
-          <div className="block justify-center">
-            <div className="flex justify-center">
-              <input
-                type="text"
-                placeholder="Recipient Address (0x...)"
-                value={recipient}
-                onChange={(e) => setRecipient(e.target.value)}
-                className="border p-2 mb-2 rounded w-80"
-              />
-            </div>
-            <div className="flex justify-center">
-              <input
-                type="text"
-                placeholder="Amount (ETH)"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                className="border p-2 mb-2 rounded w-80"
-              />
+          <div>
+            <div>
+              <h2 className="text-xl font-semibold mb-4 text-center">
+                Create a Biconomy Smart Account
+              </h2>
+              <div className="flex justify-center">
+                <button
+                  onClick={createSmartAccount}
+                  disabled={loading}
+                  className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition disabled:bg-gray-400"
+                >
+                  {loading ? "Processing..." : "Create Smart Account"}
+                </button>
+              </div>
+              {smartAccount && (
+                <div className="flex justify-center mt-4">
+                  <div className="mt-4 p-3 bg-gray-100 rounded w-1/2 text-center">
+                    <p className="text-sm font-medium">{smartAddress}</p>
+                    <div></div>
+                  </div>
+                </div>
+              )}
             </div>
           </div>
-          <div className="flex justify-center mt-4">
-            <button
-              onClick={handleTransaction}
-              disabled={loading}
-              className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition disabled:bg-gray-400"
-            >
-              {loading ? "Processing..." : "Send Gasless Transaction"}
-            </button>
-          </div>
-
-          {transactionHash && (
-            <div className="mt-4 p-3 bg-gray-100 rounded w-80 text-center">
-              <p className="text-sm font-medium">Transaction Hash:</p>
-              <a
-                href={`https://sepolia.basescan.org/tx/${transactionHash}`}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600 break-all"
+          <div>
+            <h2 className="text-xl font-semibold mb-4 text-center mt-8">
+              Send Gasless Transaction
+            </h2>
+            <div className="block justify-center">
+              <div className="flex justify-center">
+                <input
+                  type="text"
+                  placeholder="Recipient Address (0x...)"
+                  value={recipient}
+                  onChange={(e) => setRecipient(e.target.value)}
+                  className="border p-2 mb-2 rounded w-80"
+                />
+              </div>
+              <div className="flex justify-center">
+                <input
+                  type="text"
+                  placeholder="Amount (ETH)"
+                  disabled
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                  className="border p-2 mb-2 rounded w-80"
+                />
+              </div>
+            </div>
+            <div className="flex justify-center mt-4">
+              <button
+                onClick={handleTransaction}
+                disabled={loading}
+                className="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 transition disabled:bg-gray-400"
               >
-                {transactionHash}
-              </a>
+                {loading ? "Processing..." : "Send Gasless Transaction"}
+              </button>
             </div>
-          )}
-        </div>{" "}
+
+            {transactionHash && (
+              <div className="flex justify-center mt-4">
+                <div className="mt-4 p-3 bg-gray-100 rounded w-80 text-center">
+                  <p className="text-sm font-medium">Transaction Hash:</p>
+                  <a
+                    href={`https://sepolia.basescan.org/tx/${transactionHash}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="text-blue-600 break-all"
+                  >
+                    {transactionHash}
+                  </a>
+                </div>
+              </div>
+            )}
+          </div>{" "}
+        </div>
       </ParaEvmProvider>
     </QueryClientProvider>
   );
